829620c358899261f2478c966aa6044ae6f2c29a
Diana Badas <diana.badas@gmail.com>
2019-07-27 21:17:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function wrapGifts(gifts) {
 }
 
 wrapGifts(gifts);
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift.`)
+  }
+}
